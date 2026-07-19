292c508f20be420c28b5b9a4163fb4f01a18dda2
shubhashkumar <subhashsharmandhruv@gmail.com>
2026-07-19 12:16:50 +0530

    getElementByTagName modification

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Write your code below:
+/*
 const fruits = document.getElementsByClassName("fruit");
 for (let i = 0; i < fruits.length; i++)
 {
     fruits[i].style.fontWeight = "bold";
     if (i == 2)
         fruits[i].style.backgroundColor = "yellow";
+} 
+    */
+
+// Write your code below:
+const liTagElem = document.getElementsByTagName("li");
+for (let i = 0; i < liTagElem.length; i++)
+{
+    liTagElem[i].style.fontStyle = "italic";
+    if (i == liTagElem.length - 1)
+        liTagElem[i].style.color = "red";
 }
